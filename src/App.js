f2c9a2d39add8af438809dceb57e77105db013f6
Aniruddh Premdas <aniruddhstar@yahoo.com>
2020-06-14 23:12:22 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  
+import React from "react";
+import { Nav } from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AddTrip } from "./components/trip/AddTrip";
+import { TripList } from "./components/trip/TripList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTrips: [
+        {
+          place: "Delhi",
+          date: "2020-06-06",
+          type: "Club",
+        },
+        {
+          place: "Rajasthan",
+          date: "2020-06-07",
+          type: "Club",
+        },
+      ],
+    };
+    this.addTrip = this.addTrip.bind(this);
+  }
+
+  addTrip(newTrip) {
+    this.setState((prevState) => {
+      return {
+        allTrips: [...prevState.allTrips, newTrip],
+      };
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="route-container">
+          <Nav />
+          <Switch>
+            <Route
+              path="/list"
+              render={(props) => (
+                <TripList {...props} trips={this.state.allTrips} />
+              )}
+            />
+            <Route
+              path="/add"
+              render={(props) => (
+                <AddTrip {...props} AddNewTrip={this.addTrip} />
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
+
+// Components
+//  -> functional based
+//  -> class based
 
 export default App;
